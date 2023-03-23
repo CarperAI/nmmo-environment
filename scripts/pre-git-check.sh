d6cf78acec2206d8b9ff3070656dc8a19485fc5a
Kyoung Whan Choe <choe.kyoung@gmail.com>
2023-03-22 20:40:05 -0700

    renamed pylint.cfg so that pylint can run without --rcfile

--- scripts/pre-git-check.sh
+++ scripts/pre-git-check.sh
@@ -4,26 +4,26 @@ echo
 echo "Checking pylint, xcxc, pytest without touching git"
 echo
 
-# Run linter
-echo "--------------------------------------------------------------------"
-echo "Running linter..."
-if ! pylint --rcfile=pylint.cfg --fail-under=10 $(git ls-files '*.py'); then
-    echo "Lint failed. Exiting."
-    exit 1
-fi
-
 # Check if there are any "xcxc" strings in the code
 echo "--------------------------------------------------------------------"
-echo "Looking for xcxc..."
-files=$(find . -name '*.py')
+echo "Running linter & Looking for xcxc on modified files only..."
+files=$(git ls-files -m -o --exclude-standard '*.py')
 for file in $files; do
-    if grep -q 'xcxc' $file; then
-        echo "Found xcxc in $file!" >&2
-        read -p "Do you like to stop here? (y/n) " ans
-        if [ "$ans" = "y" ]; then
-            exit 1
-        fi
+  if test -e $file; then
+    echo $file
+    if ! pylint --errors-only --fail-under=10 $file; then
+      echo "Lint failed. Exiting."
+      exit 1
     fi
+
+    if grep -q -F 'xcxc' $file; then
+      echo "Found xcxc in $file!" >&2
+      read -p "Do you like to stop here? (y/n) " ans
+      if [ "$ans" = "y" ]; then
+        exit 1
+      fi
+    fi
+  fi
 done
 
 # Run unit tests
@@ -31,8 +31,8 @@ echo
 echo "--------------------------------------------------------------------"
 echo "Running unit tests..."
 if ! pytest; then
-    echo "Unit tests failed. Exiting."
-    exit 1
+  echo "Unit tests failed. Exiting."
+  exit 1
 fi
 
 echo
